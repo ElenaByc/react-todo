438110e1a1cec398252c54447160c929275e8b9d
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-03-04 08:24:42 -0800

    feat: create AddTodoForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import TodoList from './TodoList'
+import AddTodoForm from './AddTodoForm' 
 
 function App() {
   return (
     <>
       <h1>Todo List</h1>
+      <AddTodoForm />
       <TodoList />
     </>
   )
