09b0b2b5ba9df4a60472a14b951042b811e31c9d
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-03-10 22:52:09 -0700

    feat: add handleAddTodo fucntion

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,8 +1,16 @@
 const AddTodoForm = () => {
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    const todoTitle = event.target.title.value;
+    event.target.reset();
+    console.log(todoTitle);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
-      <input type="text" id="todoTitle" />
+      <input type="text" id="todoTitle" name="title" />
       <input type="submit" value="Add" />
     </form>
   );
