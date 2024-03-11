ea73b32c0ec7d56cc68ca2a64ca6c74cf9bb172a
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-03-10 23:11:44 -0700

    feat: add new rule for eslint to ignore react/prop-types

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,8 +1,8 @@
 const TodoListItem = ({ todo }) => <li>{todo.title}</li>;
 
 // I am getting these eslint errors here: 
-// 'todo' is missing in props validation eslintreact/prop-types
-// 'todo.title' is missing in props validation eslintreact/prop-types
+// 'todo' is missing in props validation eslint react/prop-types
+// 'todo.title' is missing in props validation eslint react/prop-types
 // so, I added 'react/prop-types': 'off' to eslint rules, is there a better solution? 
 
 export default TodoListItem;
