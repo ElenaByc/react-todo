a8f7a63859aa934997fa5632dc49cc919b1738a1
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-03-22 12:42:44 -0700

    feat: add onChange prop equal to handleTitleChange, create handleTitleChange function

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 const AddTodoForm = (props) => {
+
+  const [todoTitle, setTodoTitle] = useState('');
+
+  const handleTitleChange = (event) => {
+    const newTodoTitle = event.target.value;
+    setTodoTitle(newTodoTitle);
+  };
 
   const handleAddTodo = (event) => {
     event.preventDefault();
-    const todoTitle = event.target.title.value;
     event.target.reset();
     console.log(todoTitle);
     props.onAddTodo(todoTitle);
@@ -11,7 +19,7 @@ const AddTodoForm = (props) => {
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
-      <input type="text" id="todoTitle" name="title" required />
+      <input type="text" id="todoTitle" name="title" value={todoTitle} onChange={handleTitleChange} required />
       <input type="submit" value="Add" />
     </form>
   );
